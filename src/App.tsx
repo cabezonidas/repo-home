48852ae4e8905a7e3f00def90f80832013c889d0
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-04-24 11:33:13 +1200

    Add header title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ResponsiveLayout, useTranslation } from "@cabezonidas/shop-ui";
 import React from "react";
 
+const enUs = {
+  title: "De las Artes",
+};
+const esAr = {
+  title: "De las Artes",
+};
+
 function App() {
-  return <div data-testid="repo-home" />;
+  const { t, i18n } = useTranslation();
+  i18n.addResourceBundle("en-US", "translation", { app: enUs }, true, true);
+  i18n.addResourceBundle("es-AR", "translation", { app: esAr }, true, true);
+  return <ResponsiveLayout header={t("app.title")} />;
 }
 
 export default App;
