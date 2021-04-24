5dcd43c7d887b25c208857dda56ba7540fa61026
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-04-25 11:29:36 +1200

    Update album props

--- src/componets/album.tsx
+++ src/componets/album.tsx
@@ -3,31 +3,23 @@ import React from "react";
 export const Album = React.forwardRef<
   HTMLAnchorElement,
   React.AnchorHTMLAttributes<HTMLAnchorElement> & {
-    album: string;
-    title: string;
-    farm: number;
-    primary: string;
-    secret: string;
+    link: string;
+    cover: string;
     width?: number;
     height?: number;
   }
->(({ album, farm, title, primary, secret, width = 640, height = 480, ...props }, ref) => {
+>(({ title, link, cover, width = 640, height = 480, ...props }, ref) => {
   return (
     <a
       ref={ref}
       data-flickr-embed="true"
       data-header="false"
       data-footer="false"
-      href={`https://www.flickr.com/photos/lareposteria/albums/${album}`}
+      href={link}
       title={title}
       {...props}
     >
-      <img
-        src={`https://live.staticflickr.com/${farm}/${primary}_${secret}.jpg`}
-        width={width}
-        height={height}
-        alt={title}
-      />
+      <img src={cover} width={width} height={height} alt={title} />
     </a>
   );
 });
