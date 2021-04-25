91a367df64e6c9b92954080f92ca0bf1209a23a0
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-04-25 20:35:21 +1200

    Render products section and add social media links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import { Box, ResponsiveLayout, useTranslation } from "@cabezonidas/shop-ui";
-import React from "react";
+import {
+  Box,
+  Email,
+  Facebook,
+  FooterLink,
+  Instagram,
+  Messenger,
+  NavLink,
+  ResponsiveLayout,
+  useTranslation,
+  Whatsapp,
+} from "@cabezonidas/shop-ui";
 import { Gallery } from "./componets/gallery";
+import { Info } from "./componets/info";
 import { LandingSection } from "./componets/landing-section";
 
 const enUs = {
@@ -14,10 +25,69 @@ function App() {
   const { t, i18n } = useTranslation();
   i18n.addResourceBundle("en-US", "translation", { app: enUs }, true, true);
   i18n.addResourceBundle("es-AR", "translation", { app: esAr }, true, true);
+
   return (
-    <ResponsiveLayout header={<Box data-testid="repo-header">{t("app.title")}</Box>}>
+    <ResponsiveLayout
+      header={<Box data-testid="repo-header">{t("app.title")}</Box>}
+      nav={
+        <>
+          <NavLink css={{}} href="https://api.whatsapp.com/send?phone=5491127778899">
+            <Box display="grid" gridTemplateColumns="auto 1fr" gridGap="4">
+              <Whatsapp css={{}} />
+              <Box>11 2777 8899</Box>
+            </Box>
+          </NavLink>
+          <NavLink css={{}} href="https://www.instagram.com/ReposteriaDeLasArtes">
+            <Box display="grid" gridTemplateColumns="auto 1fr" gridGap="4">
+              <Instagram css={{}} />
+              <Box>Instagram</Box>
+            </Box>
+          </NavLink>
+          <NavLink css={{}} href="https://www.facebook.com/ReposteriaDeLasArtes">
+            <Box display="grid" gridTemplateColumns="auto 1fr" gridGap="4">
+              <Facebook css={{}} />
+              <Box>Facebook</Box>
+            </Box>
+          </NavLink>
+          <NavLink css={{}} href="https://www.messenger.com/t/ReposteriaDeLasArtes">
+            <Box display="grid" gridTemplateColumns="auto 1fr" gridGap="4">
+              <Messenger css={{}} />
+              <Box>Messenger</Box>
+            </Box>
+          </NavLink>
+        </>
+      }
+      footer={
+        <Box display="flex" justifyContent="space-between">
+          <Box
+            display="grid"
+            gridTemplateColumns="auto 50px 50px 50px 50px 50px"
+            width="100%"
+            alignItems="center"
+          >
+            <Box />
+            <FooterLink css={{}} href="mailto:info@delasartes.com.ar">
+              <Email css={{}} />
+            </FooterLink>
+            <FooterLink css={{}} href="https://api.whatsapp.com/send?phone=5491127778899">
+              <Whatsapp css={{}} />
+            </FooterLink>
+            <FooterLink css={{}} href="https://www.instagram.com/ReposteriaDeLasArtes">
+              <Instagram css={{}} />
+            </FooterLink>
+            <FooterLink css={{}} href="https://www.facebook.com/ReposteriaDeLasArtes">
+              <Facebook css={{}} />
+            </FooterLink>
+            <FooterLink css={{}} href="https://www.messenger.com/t/ReposteriaDeLasArtes">
+              <Messenger css={{}} />
+            </FooterLink>
+          </Box>
+        </Box>
+      }
+    >
       <LandingSection />
-      <Gallery />
+      <Info />
+      <Gallery maxWidth="90%" mx="auto" mb="100px" />
     </ResponsiveLayout>
   );
 }
