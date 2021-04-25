072f6bc62787dad5cdc3eff00917bc56ce822166
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-04-25 23:18:50 +1200

    Add loading state

--- src/componets/products.tsx
+++ src/componets/products.tsx
@@ -9,7 +9,7 @@ export const Products = React.forwardRef<HTMLDivElement, React.ComponentProps<ty
     const { isLarge, isMediumLarge } = useBreakpoint();
 
     const fontSizeHeading = isLarge ? 100 : isMediumLarge ? 80 : 55;
-    const { data } = useProducts();
+    const { data, isLoading, isSuccess } = useProducts();
 
     type Item = NonNullable<typeof data>[number]["items"][number];
 
@@ -33,42 +33,45 @@ export const Products = React.forwardRef<HTMLDivElement, React.ComponentProps<ty
         <Box mb="2" textAlign="center">
           Recuerda escribirnos a nuestro WhatsApp para concretar un pedido
         </Box>
-        <Box px="14px" overflow="hidden">
-          <Box maxWidth="500px" mb="4" mx="auto">
-            <Label htmlFor="search" css={{}}>
-              Buscar
-            </Label>
-            <Input
-              css={{}}
-              id="search"
-              value={filterKey}
-              onChange={e => setFilterKey(e.target.value)}
-              placeholder="pastel del sur"
-            />
-          </Box>
-          <Box height="400px" overflow="hidden">
-            <Box overflow="auto" height="100%" maxWidth="500px" mx="auto">
-              <Box
-                display="grid"
-                gridTemplateColumns="1fr auto"
-                gridTemplateRows="max-content"
-                style={{ rowGap: 5 }}
-              >
-                {sortedItems.map(item => (
-                  <React.Fragment key={item.productId}>
-                    <Box>
-                      <Box fontWeight="bold">{item.displayName}</Box>
-                      <Box fontSize="small">{item.displayDescription}</Box>
-                    </Box>
-                    <Box textAlign="right" fontSize="large">
-                      {item.price}
-                    </Box>
-                  </React.Fragment>
-                ))}
+        {isLoading && <Box>Cargando...</Box>}
+        {isSuccess && (
+          <Box px="14px" overflow="hidden">
+            <Box maxWidth="500px" mb="4" mx="auto">
+              <Label htmlFor="search" css={{}}>
+                Buscar
+              </Label>
+              <Input
+                css={{}}
+                id="search"
+                value={filterKey}
+                onChange={e => setFilterKey(e.target.value)}
+                placeholder="pastel del sur"
+              />
+            </Box>
+            <Box height="400px" overflow="hidden">
+              <Box overflow="auto" height="100%" maxWidth="500px" mx="auto">
+                <Box
+                  display="grid"
+                  gridTemplateColumns="1fr auto"
+                  gridTemplateRows="max-content"
+                  style={{ rowGap: 5 }}
+                >
+                  {sortedItems.map(item => (
+                    <React.Fragment key={item.productId}>
+                      <Box>
+                        <Box fontWeight="bold">{item.displayName}</Box>
+                        <Box fontSize="small">{item.displayDescription}</Box>
+                      </Box>
+                      <Box textAlign="right" fontSize="large">
+                        {item.price}
+                      </Box>
+                    </React.Fragment>
+                  ))}
+                </Box>
               </Box>
             </Box>
           </Box>
-        </Box>
+        )}
       </Box>
     );
   }
