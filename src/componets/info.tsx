b5731bea6208214e0ae3e273c06777003e2c0fa7
Sebastian Cabeza <sebastian.cabeza@noted.com>
2023-01-16 09:56:58 -0300

    remove emojis;

--- src/componets/info.tsx
+++ src/componets/info.tsx
@@ -1,29 +1,12 @@
-import { Box, useBreakpoint } from "@cabezonidas/shop-ui";
+import { Box } from "@cabezonidas/shop-ui";
 import styled from "@cabezonidas/shop-ui/lib/theme/styled";
 import React from "react";
 import { Products } from "./products";
 
 export const Info = React.forwardRef<HTMLDivElement, React.ComponentProps<typeof Container>>(
   ({ children, ...props }, ref) => {
-    const { isMediumSmall } = useBreakpoint();
-
     return (
       <Container ref={ref} {...props}>
-        <Box
-          fontSize={isMediumSmall ? "50px" : "30px"}
-          display="grid"
-          gridTemplateColumns={`repeat(4, ${isMediumSmall ? "100px" : "65px"})`}
-          width="max-content"
-          mx="auto"
-          mt="50px"
-          textAlign="center"
-        >
-          <Box>🍰</Box>
-          <Box>🥧</Box>
-          <Box>🎂</Box>
-          <Box>🍪</Box>
-        </Box>
-
         <Products />
         {children}
       </Container>
